4bcfeed86e74d713569832322e908bac7e9c0c2e
Apoorv-123 <36572816+Apoorv-123@users.noreply.github.com>
2018-02-26 22:41:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-second', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-third', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
